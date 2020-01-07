93715c67ebfa84d7494d1e026682d1b40a451d8a
Charlon <charles.assus@gmail.com>
2020-01-07 10:29:10 +0100

    Remove Binding.pry

--- app/graphql/portfolio_states/service.rb
+++ app/graphql/portfolio_states/service.rb
@@ -12,7 +12,6 @@ module PortfolioStates
       p.allocations.each do |a|
         tab[a.symbol] = HTTParty.get("https://api.worldtradingdata.com/api/v1/history?symbol=#{a.symbol}&date_from=#{params[:start_date]}&api_token=" + ENV['WORLD_TRADING_DATA_KEY'])
         if tab[a.symbol]['Message'].blank?
-          binding.pry
           final_balance += p.initial_balance * a.percentage * tab[a.symbol]['history'].values.first['close'].to_f / (100 * tab[a.symbol]['history'].values.last['close'].to_f)
         end
       end
