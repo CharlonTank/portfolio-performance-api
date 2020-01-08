28e42ae06050a92e1e0a5c39692fc802031e5f1a
Charlon <charles.assus@gmail.com>
2020-01-08 03:16:40 +0100

    Fix bug where I can't create new portfolio

--- app/graphql/portfolio_states/service.rb
+++ app/graphql/portfolio_states/service.rb
@@ -1,6 +1,6 @@
 module PortfolioStates
   class Service < ApplicationService
-    
+
     def create 
       if params[:save]
         p = PortfolioState.find_by(id: params[:token])
@@ -13,13 +13,36 @@ module PortfolioStates
                  initial_balance: params[:initial_balance])
       end
       tab = {}
-      final_balance = 0
       p_openstruct = OpenStruct.new(final_balance: 0, allocations: [])
       params[:allocations].each do |a|
         tab[a['symbol']] = HTTParty.get("https://api.worldtradingdata.com/api/v1/history?symbol=#{a['symbol']}&date_from=#{params[:start_date]}&api_token=" + ENV['WORLD_TRADING_DATA_KEY'])
         next unless tab[a['symbol']]['Message'].blank?
 
-        final_balance += params[:initial_balance] * a['percentage'] * tab[a['symbol']]['history'].values.first['close'].to_f / (100 * tab[a['symbol']]['history'].values.last['close'].to_f)
+        p_openstruct[:final_balance] += params[:initial_balance] * a['percentage'] * tab[a['symbol']]['history'].values.first['close'].to_f / (100 * tab[a['symbol']]['history'].values.last['close'].to_f)
+        al_openstruct = OpenStruct.new(symbol: a['symbol'],
+                                       percentage: a['percentage'],
+                                       price_per_times: [])
+        tab[a['symbol']]['history'].map do |k, v|
+          al_openstruct[:price_per_times] <<
+          OpenStruct.new(value: v['close'], time: k)
+        end
+        p_openstruct.allocations << al_openstruct
+      end
+      p_openstruct[:token] = p&.id
+      p_openstruct[:start_date] = params[:start_date]
+      p_openstruct[:initial_balance] = params[:initial_balance]
+      p_openstruct
+    end
+
+    def show
+      p = PortfolioState.find_by(id: params[:id])
+      p_openstruct = OpenStruct.new(final_balance: 0, allocations: [])
+      tab = {}
+      p&.allocations&.each do |a|
+        tab[a['symbol']] = HTTParty.get("https://api.worldtradingdata.com/api/v1/history?symbol=#{a['symbol']}&date_from=#{p&.start_date}&api_token=" + ENV['WORLD_TRADING_DATA_KEY'])
+        next unless tab[a['symbol']]['Message'].blank?
+
+        p_openstruct[:final_balance] += p.initial_balance * a['percentage'] * tab[a['symbol']]['history'].values.first['close'].to_f / (100 * tab[a['symbol']]['history'].values.last['close'].to_f)
         al_openstruct = OpenStruct.new(symbol: a['symbol'],
                                        percentage: a['percentage'],
                                        price_per_times: [])
@@ -29,9 +52,11 @@ module PortfolioStates
         end
         p_openstruct.allocations << al_openstruct
       end
-      p_openstruct[:final_balance] = final_balance
       p_openstruct[:token] = p&.id
+      p_openstruct[:start_date] = p&.start_date
+      p_openstruct[:initial_balance] = p&.initial_balance
       p_openstruct
     end
   end
+
 end
--- app/graphql/portfolio_states/type.rb
+++ app/graphql/portfolio_states/type.rb
@@ -2,6 +2,8 @@ PortfolioStates::Type = GraphQL::ObjectType.define do
   name 'PortfolioState'
   field :id, !types.String
   field :final_balance, !types.Int
+  field :start_date, !types.String
+  field :initial_balance, !types.Int
   field :allocations, !types[!Allocations::Type]
   field :token, types.String
 end
